80214022bcef8c89d3d67604fc3317686d6b6700
Diego Sullon <67489081+DiegoSullon@users.noreply.github.com>
2025-05-22 22:01:49 -0500

    fix module path

--- infra/main.tf
+++ infra/main.tf
@@ -6,14 +6,14 @@ terraform {
   }
 }
 module "s3" {
-  source    = "./modules/s3"
+  source    = "../modules/s3"
   buckets   = local.buckets
   proyecto  = var.proyecto
   ambiente  = var.ambiente
 }
 
 module "iam" {
-  source      = "./modules/iam"
+  source      = "../modules/iam"
   role_name   = var.glue_role_name
   policy_name = var.glue_policy_name
 }
